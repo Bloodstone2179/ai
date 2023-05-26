0ec60c70ffc341d60e96ff9d0f4757bbffb970d6
Bloodstone2179 <colinfrurze@gmail.com>
2023-05-26 19:48:07 +0100

    Update AI.py

--- Testing V2/AI.py
+++ Testing V2/AI.py
@@ -5,23 +5,31 @@ import numpy as np
 import csv, queue
 from thread_custom import Thread
 #variables
-global bbox, currentImg, ghostImg
+global bbox, currentImg, ghosts
 bbox=(672,261,1267,912)
 currentImg = None
 detectBuffer = queue.Queue()
-ghostImg = cv.imread("color-0.png", cv.IMREAD_GRAYSCALE)
+show_buffer = queue.Queue()
+red = cv.cvtColor(cv.imread("color-2.png", cv.IMREAD_UNCHANGED), cv.COLOR_BGR2HSV)
+blue = cv.cvtColor(cv.imread("color-3.png", cv.IMREAD_UNCHANGED), cv.COLOR_BGR2HSV)
+pink = cv.cvtColor(cv.imread("color-1.png", cv.IMREAD_UNCHANGED), cv.COLOR_BGR2HSV)
+orange = cv.cvtColor(cv.imread("color-0.png", cv.IMREAD_UNCHANGED), cv.COLOR_BGR2HSV)
 
+baseDisplayImg =  np.ascontiguousarray(ImageGrab.grab(bbox))
+baseDisplayImg = cv.cvtColor(baseDisplayImg, cv.COLOR_BGR2HSV)
 
-
+ghosts = [red, blue, pink, orange]
 #record screen
 def record():
     while True:
         img = np.ascontiguousarray(ImageGrab.grab(bbox))
-        haystack = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        haystack = cv.cvtColor(img, cv.COLOR_BGR2HSV)
         detectBuffer.put(haystack)
-        cv.imshow("hello", img)
+        
         if cv.waitKey(1) & 0xFF == ord('q'):
             detectionThread.event.set()
+            ShowThread.event.set()
+            recordThread.event.set()
             cv.destroyAllWindows()
             break
 
@@ -31,23 +39,36 @@ def record():
 def detectGhost(event, threshold = 0.5):
     while True and not event.is_set():
         haystack = detectBuffer.get()
-        result = cv.matchTemplate(haystack, ghostImg, cv.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        if max_val >= threshold:
-            print(f"ghost found  Max Threshold:{str(round(max_val, 4))}  Max loc {str(max_loc)}" )
+        for ghost in ghosts:
+            result = cv.matchTemplate(haystack, ghostImg, cv.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+            if max_val >= threshold:
+                print(f"ghost found  Max Threshold:{str(round(max_val, 4))}  Max loc {str(max_loc)}" )
+                cv.rectangle(haystack, min_loc, max_loc, (255,255,255), cv.LINE_4)
+                show_buffer.put(haystack)
 
 #display each image and ask user what way to go
-
+def Show(event):
+    while True and not event.is_set():
+        img = show_buffer.get()
+        if img is None:
+            img = baseDisplayImg
+            print("No Image Avaliable for display using base image")
+        cv.imshow("hello", img)
 
 #print that to a file
 
 
 if __name__ == "__main__":
-    for i in range(4):
-        img = cv.imread(f"color-{i}.png", cv.IMREAD_GRAYSCALE)
-        cv.imwrite(f"grayscale-{i}.png", img)
     detectionThread = Thread(detectGhost)
     detectionThread.args = (detectionThread.event,0.6)
     detectionThread.start_()
-    record()
+
+    ShowThread = Thread(Show)
+    ShowThread.args = (ShowThread.event,0.6)
+    ShowThread.start_()
+
+    recordThread = Thread(record)
+    recordThread.args = (recordThread.event,0.6)
+    recordThread.start_()
     
